fc4f013f2d142746dc799582952e6e05e66f9d9f
gavinarori <arorigavin@gmail.com>
2025-02-02 23:33:05 +0300

    removed the banner

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,12 @@
 "use client"
+
 import { useState } from "react"
 import { AppSidebar } from "@/components/app-sidebar"
 import { Separator } from "@/components/ui/separator"
-import { Button } from "@/components/ui/button"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
-import * as React from "react"
-import { cn } from "@/lib/utils";
-import { DotPattern } from "@/components/dot-pattern";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { Chart } from "@/components/charts"
 import ProductForm from "@/components/product-form"
 import { Banner } from "@/components/banner"
-
 
 export default function Page() {
   const [activeView, setActiveView] = useState<string>("home")
@@ -34,17 +26,19 @@ export default function Page() {
     <SidebarProvider>
       <AppSidebar onNavigate={setActiveView} />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <Banner />
-            {renderActiveView()}
-        </div>
+        <main className="flex-1 overflow-auto">
+          <div className="container mx-auto px-4 py-6 sm:px-6 lg:px-8">
+            <div className="rounded-lg bg-background mt-8 shadow">{renderActiveView()}</div>
+          </div>
+        </main>
       </SidebarInset>
     </SidebarProvider>
   )
 }
+
