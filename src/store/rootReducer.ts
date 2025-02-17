3b2fc78828190a94fcfcf0f643efacc218976e96
gavinarori <arorigavin@gmail.com>
2025-02-17 14:56:13 +0300

    added: the chat reducer and seller and admin file structure

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,6 +1,8 @@
-import authReducer from './Reducers/authReducer'
+import authReducer from './Reducers/authReducer';
+import chatReducer from './Reducers/chatReducer'
 
 const rootReducer = {
     auth: authReducer,
+    chat: chatReducer,
 }
 export default rootReducer;
