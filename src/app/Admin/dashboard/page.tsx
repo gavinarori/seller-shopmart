84a0578d1bf95ad2c972b52496b8d92bd04f2c96
gavinarori <arorigavin@gmail.com>
2025-02-23 14:02:53 +0300

    updated: added the category-add and ui and tested it on the admin

--- src/app/Admin/dashboard/page.tsx
+++ src/app/Admin/dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { AppSidebar } from "@/components/Admin-components/app-sidebar"
 import { Separator } from "@/components/ui/separator"
 import {
@@ -5,24 +7,36 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { CategoryForm } from "@/components/Admin-components/catergory-form"
+import { AdminHome } from "@/components/Admin-components/Home"
+import { useState } from "react"
 
 export default function Page() {
+  const [activeView, setActiveView] = useState<string>("Admin-home")
+
+  const renderActiveView = () => {
+    switch (activeView) {
+      case "Admin-home":
+        return <AdminHome  />
+      case "Category":
+        return <CategoryForm onCancel={() => setActiveView("Admin-home")} />
+      default:
+        return <AdminHome  />
+    }
+  }
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar onNavigate={setActiveView}/>
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+        <main className="flex-1 overflow-auto">
+          <div className="container mx-auto px-4 py-6 sm:px-6 lg:px-8">
+            <div className="rounded-lg bg-background mt-8 shadow">{renderActiveView()}</div>
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div>
+        </main>
       </SidebarInset>
     </SidebarProvider>
   )
